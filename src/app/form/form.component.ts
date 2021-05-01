96956c9c9b2ccfa896c73d7b88c657b31582c7df
Ilya <ilyafateev@borrowlabs.com>
2021-05-01 14:31:49 +0300

    Отправка формы (вывод в concole.log)

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormControlName, FormGroup } from '@angular/forms';
+import {FormArray, FormControl, FormControlName, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,60 +9,83 @@ import { FormControl, FormControlName, FormGroup } from '@angular/forms';
 export class FormComponent implements OnInit {
   users = [
     {
+      id: 1,
       name: 'Темирлан Темирбиев',
       position: 'Супервайзер',
-      img: 'man.png',
-      check: false
+      img: 'man.png'
     },
     {
+      id: 2,
       name: 'Анастасия Морозова',
       position: 'Тестировщик',
-      img: 'woman.png',
-      check: false
+      img: 'woman.png'
     },
     {
+      id: 3,
       name: 'Александр Пушкин',
       position: 'Разработчик',
-      img: 'grand.png',
-      check: false
+      img: 'grand.png'
     },
     {
+      id: 4,
       name: 'Темирлан Темирбиев',
       position: 'Супервайзер',
-      img: 'man.png',
-      check: false
+      img: 'man.png'
     },
     {
+      id: 5,
       name: 'Темирлан Темирбиев',
       position: 'Супервайзер',
-      img: 'man.png',
-      check: false
+      img: 'man.png'
     },
     {
+      id: 6,
       name: 'Темирлан Темирбиев',
       position: 'Супервайзер',
-      img: 'man.png',
-      check: false
+      img: 'man.png'
     },
     {
+      id: 7,
       name: 'Темирлан Темирбиев',
       position: 'Супервайзер',
-      img: 'man.png',
-      check: false
+      img: 'man.png'
     },
     {
+      id: 8,
       name: 'Темирлан Темирбиев',
       position: 'Супервайзер',
-      img: 'man.png',
-      check: false
+      img: 'man.png'
     }
   ];
 
+  public sentArray = [];
+
   public myForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.myForm = new FormGroup({
+      users: new FormArray([])
+    });
   }
 
+  ngOnInit(): void {
+    this.users.forEach(user => {
+      (this.myForm.get('users') as FormArray).push(new FormGroup({
+        id: new FormControl(user.id),
+        selected: new FormControl(false),
+        name: new FormControl(user.name),
+        position: new FormControl(user.position)
+      }))
+    });
+  }
+
+  cancel() {
+    this.myForm.reset();
+  }
+
+  send() {
+    if (this.myForm.valid) {
+        console.log((this.myForm.get('users').value as []).filter(user => user['selected']), 'Массив отправки');
+    }
+  }
 }
